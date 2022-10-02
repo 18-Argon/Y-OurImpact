1644e809947dc23755107f65475133b0c488c58d
Aryan Gondkar <104461910+18-Argon@users.noreply.github.com>
2022-10-02 01:57:44 -0400

    Add files via upload

--- ml_data_generator.py
+++ ml_data_generator.py
@@ -6,7 +6,7 @@ pareto_front=list(front)
 
 for F1 in pareto_front:
     for F2 in pareto_front:
-        if (F1<=F2).all() and (F1<F2).any():
+        if (F1[1::]<=F2[1::]).all() and (F1[1::]<F2[1::]).any():
             pareto_front=[F for F in pareto_front if not (F==F2).all()]
     
 print(front==pareto_front)
