ace84a0a6a813f25e40e3fe73e67e2807fdfd78e
Aryan Gondkar <104461910+18-Argon@users.noreply.github.com>
2022-10-02 02:27:33 -0400

    Add files via upload

--- ml_data_generator.py
+++ ml_data_generator.py
@@ -1,5 +1,6 @@
 from fake_market import cities, suppliers, objects
 import numpy as np
+import random
 
 front=[obj.F for obj in objects]
 pareto_front=list(front)
@@ -13,4 +14,14 @@ for F1 in pareto_front:
 
 print(front==pareto_front)
 print(len(dominated_front)+len(pareto_front), len(front))
+
+training_set= [(pareto_front[i], 1) for i in range(16)]
+training_set=training_set+[(dominated_front[i], 0) for i in range(16)]
+random.shuffle(training_set)
+training_set_x, training_set_y = [x[0] for x in training_set], [y[1] for y in training_set]
+
+test_set= [(pareto_front[i], 1) for i in range(16,21)]
+test_set=test_set+[(dominated_front[i], 0) for i in range(16,21)]
+random.shuffle(test_set)
+test_set_x, test_set_y = [x[0] for x in test_set], [y[1] for y in test_set]
 print()
