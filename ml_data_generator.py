eb0518d3a908c861acd84557393aa038d5e9b3a6
Aryan Gondkar <104461910+18-Argon@users.noreply.github.com>
2022-10-02 02:16:52 -0400

    Add files via upload

--- ml_data_generator.py
+++ ml_data_generator.py
@@ -3,11 +3,14 @@ import numpy as np
 
 front=[obj.F for obj in objects]
 pareto_front=list(front)
+dominated_front=[]
 
 for F1 in pareto_front:
     for F2 in pareto_front:
         if (F1[1::]<=F2[1::]).all() and (F1[1::]<F2[1::]).any():
-            pareto_front=[F for F in pareto_front if not (F==F2).all()]
-    
+            pareto_front=[F for F in pareto_front if not (F[0]==F2[0])]
+            dominated_front.append(F2)
+
 print(front==pareto_front)
+print(len(dominated_front)+len(pareto_front), len(front))
 print()
